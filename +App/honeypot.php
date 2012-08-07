06d25e923f900264cb2aa19e5a30b6afba6f6c34
srcspider <source.spider@gmail.com>
2012-08-07 11:32:28 +0300

    honeypot

--- +App/honeypot.php
+++ +App/honeypot.php
@@ -1,5 +1,25 @@
 <?php namespace app;
 
-// This is a IDE honeypot. :)
+// This is an IDE honeypot. It tells IDEs the class hirarchy, but otherwise has
+// no effect on your application. :)
 
-// HowTo: minion honeypot -n 'ibidem\database'
+// HowTo: order honeypot -n 'ibidem\html'
+
+class Form extends \ibidem\html\Form { /** @return \ibidem\html\Form */ static function instance($id = null) { return parent::instance($id); } }
+class FormField_Checkbox extends \ibidem\html\FormField_Checkbox { /** @return \ibidem\html\FormField_Checkbox */ static function instance($title = null, $name = null, $form = null) { return parent::instance($title, $name, $form); } }
+class FormField_DateTime extends \ibidem\html\FormField_DateTime { /** @return \ibidem\html\FormField_DateTime */ static function instance($title = null, $name = null, $form = null) { return parent::instance($title, $name, $form); } }
+class FormField_Email extends \ibidem\html\FormField_Email { /** @return \ibidem\html\FormField_Email */ static function instance($title = null, $name = null, $form = null) { return parent::instance($title, $name, $form); } }
+class FormField_File extends \ibidem\html\FormField_File { /** @return \ibidem\html\FormField_File */ static function instance($title = null, $name = null, $form = null) { return parent::instance($title, $name, $form); } }
+class FormField_Hidden extends \ibidem\html\FormField_Hidden { /** @return \ibidem\html\FormField_Hidden */ static function instance($name = null, $form = null) { return parent::instance($name, $form); } }
+class FormField_Password extends \ibidem\html\FormField_Password { /** @return \ibidem\html\FormField_Password */ static function instance($title = null, $name = null, $form = null) { return parent::instance($title, $name, $form); } }
+class FormField_Radio extends \ibidem\html\FormField_Radio { /** @return \ibidem\html\FormField_Radio */ static function instance($title = null, $name = null, $form = null) { return parent::instance($title, $name, $form); } }
+class FormField_Select extends \ibidem\html\FormField_Select { /** @return \ibidem\html\FormField_Select */ static function instance($title = null, $name = null, $form = null) { return parent::instance($title, $name, $form); } }
+class FormField_Submit extends \ibidem\html\FormField_Submit { /** @return \ibidem\html\FormField_Submit */ static function instance($title = null, $name = null, $form = null) { return parent::instance($title, $name, $form); } }
+class FormField_Telephone extends \ibidem\html\FormField_Telephone { /** @return \ibidem\html\FormField_Telephone */ static function instance() { return parent::instance(); } }
+class FormField_Text extends \ibidem\html\FormField_Text { /** @return \ibidem\html\FormField_Text */ static function instance($title = null, $name = null, $form = null) { return parent::instance($title, $name, $form); } }
+class FormField_TextArea extends \ibidem\html\FormField_TextArea { /** @return \ibidem\html\FormField_TextArea */ static function instance($title = null, $name = null, $form = null) { return parent::instance($title, $name, $form); } }
+class FormField extends \ibidem\html\FormField { /** @return \ibidem\html\FormField */ static function instance($title = null, $name = null, $form = null) { return parent::instance($title, $name, $form); } }
+class HTMLBlockElement extends \ibidem\html\HTMLBlockElement { /** @return \ibidem\html\HTMLBlockElement */ static function instance($name = 'div', $body = '') { return parent::instance($name, $body); } }
+class HTMLElement extends \ibidem\html\HTMLElement { /** @return \ibidem\html\HTMLElement */ static function instance($name = 'hr') { return parent::instance($name); } }
+class Layer_HTML extends \ibidem\html\Layer_HTML { /** @return \ibidem\html\Layer_HTML */ static function instance() { return parent::instance(); } }
+class Pager extends \ibidem\html\Pager { /** @return \ibidem\html\Pager */ static function instance($totalitems = 0, $url_base = '', $pagediff = 3, $pagelimit = 20) { return parent::instance($totalitems, $url_base, $pagediff, $pagelimit); } }
