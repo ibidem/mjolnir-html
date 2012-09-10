72a3e5c702df7f4c0a94380d614344abd03a6165
srcspider <source.spider@gmail.com>
2012-09-10 03:58:28 +0300

    refactored names to mjolnir

---
+++ +App/config/mjolnir/pager.php
@@ -0,0 +1,9 @@
+<?php return array
+	(
+		'view.default' => 'views/ibidem/pager',
+		'view.standards' => array
+			(
+				'twitter' => 'views/ibidem/pager.twitter',
+				'nopages' => 'views/ibidem/pager.nopages'
+			),
+	);
