3a226c56a7d9e638eb9bbda8bbe91fa32eb3a9ab
srcspider <source.spider@gmail.com>
2012-10-02 19:44:55 +0300

    checkbox value will now correctly set the checkbox checked state

--- FormField/Checkbox.php
+++ FormField/Checkbox.php
@@ -31,5 +31,33 @@ class FormField_Checkbox extends \app\FormField
 		
 		return $this;
 	}
+	
+	/**
+	 * @return \app\FormField_Checkbox $this
+	 */
+	function value($value)
+	{
+		if (\is_bool($value))
+		{
+			$this->checked($value);
+		}
+		else if (\in_array($value, ['1', 'on', 'yes']))
+		{
+			$this->checked(true);
+		}
+		else if (\in_array($value, ['0', 'off', 'no']))
+		{
+			$this->checked(false);
+		}
+		else # unknown
+		{
+			// checkboxes are usually in very critical places; we don't want any 
+			// ambigous handling that can totally fuck up in a spectacular way
+			// such as show "no access" instead of "allowed" in a ACL
+			throw new \app\Exception('Value can not be interpreted.');
+		}
+		
+		return $this;
+	}
 
 } # class
