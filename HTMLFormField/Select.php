b3c7560adeeca919e48402a051f4cf44ca3580fc
srcspider <source.spider@gmail.com>
2013-09-24 17:02:37 +0300

    select options_liefhierarchy and options_logical
    Basic support for raw html hierarchical structures.
    For prettier results the recomendation is to use javascript
    at the theme level; which we currently do not provide direct
    support in the library.

--- HTMLFormField/Select.php
+++ HTMLFormField/Select.php
@@ -22,6 +22,16 @@ class HTMLFormField_Select extends \app\HTMLFormField implements \mjolnir\types\
 	protected $optgroups = null;
 
 	/**
+	 * @var array
+	 */
+	protected $logicaloptions = null;
+
+	/**
+	 * @var array
+	 */
+	protected $liefhierarchy = null;
+
+	/**
 	 * @return static
 	 */
 	static function instance()
@@ -42,6 +52,48 @@ class HTMLFormField_Select extends \app\HTMLFormField implements \mjolnir\types\
 	function options_array(array $array = null)
 	{
 		$this->options = $array;
+		return $this;
+	}
+
+	/**
+	 * The hierarchy will be rendered by disabling entries that point to null.
+	 * You may provide intending to the items and set categories to null to
+	 * achieve a simple hierarchy system. The &mdash; is recomended for
+	 * indentation.
+	 *
+	 * For pretty displaying it's recomended you parse the options and do a
+	 * javascript replacement of the select field.
+	 *
+	 * @return static $this
+	 */
+	function options_logical(array $hierarchy = null)
+	{
+		$this->logicaloptions = $hierarchy;
+		return $this;
+	}
+
+	/**
+	 * In a lief hierarchy only the liefs are selectable. You specify the
+	 * hierarchy though an array with inner arrays. If a key points to an
+	 * arrays it is considered a group label, otherwise it's rendered as an
+	 * option.
+	 *
+	 * The hierarchy will be rendered by disabling category labels so it
+	 * appears consistent (unlike the empty optgroup method).
+	 *
+	 * The items are expected to be already indented.
+	 * Using &mdash; is recomended for indenting leaf hierarchies.
+	 *
+	 * If you want all items to be selectable use options_logical.
+	 *
+	 * For pretty displaying it's recomended you parse the options and do a
+	 * javascript replacement of the select field.
+	 *
+	 * @return static $this
+	 */
+	function options_liefhierarchy(array $hierarchy = null)
+	{
+		$this->liefhierarchy = $hierarchy;
 		return $this;
 	}
 
@@ -106,7 +158,69 @@ class HTMLFormField_Select extends \app\HTMLFormField implements \mjolnir\types\
 			}
 		}
 
+		if ($this->logicaloptions !== null)
+		{
+			foreach ($this->logicaloptions as $key => $label)
+			{
+				if ($label === null)
+				{
+					$option = \app\HTMLTag::i('option', $key)
+						->set('disabled', '');
+
+					$this->appendtagbody($option->render());
+				}
+				else # $label !== null
+				{
+					$option = \app\HTMLTag::i('option', $label)
+						->set('value', $key);
+
+					if ($this->values !== null && \in_array(\strval($key), $this->values, false))
+					{
+						$option->set('selected', '');
+					}
+
+					$this->appendtagbody($option->render());
+				}
+			}
+		}
+
+		if ($this->liefhierarchy !== null)
+		{
+			$this->liefhierarchy_parse_options($this->liefhierarchy);
+		}
+
 		return parent::fieldrender();
+	}
+
+	/**
+	 * Parses hierarchy input array.
+	 */
+	protected function liefhierarchy_parse_options(array $options)
+	{
+		foreach ($options as $key => $label)
+		{
+			if (\is_array($label))
+			{
+				$option = \app\HTMLTag::i('option', $key)
+					->set('disabled', '');
+
+				$this->appendtagbody($option->render());
+
+				$this->liefhierarchy_parse_options($label);
+			}
+			else # $value not array
+			{
+				$option = \app\HTMLTag::i('option', $label)
+					->set('value', $key);
+
+				if ($this->values !== null && \in_array(\strval($key), $this->values, false))
+				{
+					$option->set('selected', '');
+				}
+
+				$this->appendtagbody($option->render());
+			}
+		}
 	}
 
 } # class
